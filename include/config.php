f655e581c0c5fe0696d58ec714db869c210df2ad
pdargham <pdargham@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-27 08:53:39 +0000

    tagging version 1.3.2
    git-svn-id: https://plugins.svn.wordpress.org/multisite-clone-duplicator/tags/1.3.2@1146820 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/config.php
+++ include/config.php
@@ -16,6 +16,7 @@ define( 'MUCD_DOMAIN', 'multisite-clone-duplicator' );
  */
 define( 'MUCD_SLUG_NETWORK_ACTION', 'multisite-clone-duplicator' );
 define( 'MUCD_SLUG_ACTION_DUPLICATE', 'duplicate-site' );
+define( 'MUCD_SLUG_ACTION_SETTINGS', '_mucdsettings' );
 
 /**
  * Site to excude
@@ -26,3 +27,4 @@ define( 'MUCD_SITE_DUPLICATION_EXCLUDE', '' );
  * Environment constants
  */
 define( 'MUCD_PRIMARY_SITE_ID', 1 );
+define( 'MUCD_MAX_NUMBER_OF_SITE', 5000 );
